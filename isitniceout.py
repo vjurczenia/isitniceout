9d0a26b5ee5342c16929fdedeaff345e0e9db13e
Victor Jurczenia <vjurczenia@gmail.com>
2020-05-04 20:50:07 -0400

    add Jack's changes

--- isitniceout.py
+++ isitniceout.py
@@ -1,12 +1,21 @@
+"""
+Updated the windspeed and temperature limits
+Also added a geolocation pulled from the user's IP.
+"""
+
 import json
 import os
 import requests
 
-from flask import Flask, render_template
+from flask_simple_geoip import SimpleGeoIP
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
+app.config["GEOIPIFY_API_KEY"] = os.environ.get('GEOIPIFY_API_KEY')
+
+simple_geoip = SimpleGeoIP(app)
 
 nice_out_dict = {
     'Yes': {
@@ -21,8 +30,13 @@ nice_out_dict = {
     }
 }
 
+def user_loc():
+    # Retrieve geoip data for the given requester
+    geoip_data = simple_geoip.get_geoip_data()
+    return jsonify(data=geoip_data)
 
-# @app.route('/')
+
+#@app.route('/')
 def test():
     nice_out_key = 'No'
     return render_template('index.html', nice_out=nice_out_dict[nice_out_key])
@@ -35,12 +49,15 @@ def isitniceout():
     Use openweathermap API (https://openweathermap.org/current)
     to determine whether it's nice out.
     """
-    location_id = 5110302
+    location_dict = user_loc()
 
-    base_url = 'https://api.openweathermap.org/data/2.5/weather'
+    #location_id = 5110302
+    # lat = 35 & lon = 139
+    base_url = 'api.openweathermap.org/data/2.5/weather'
     parameters = (
         f'appid={OPENWEATHERMAP_API_KEY}'
-        f'&id={location_id}'
+        f'&lat={location_dict["lat"]}'
+        f'&lon={location_dict["lng"]}'
         '&units=imperial'
     )
 
@@ -52,8 +69,8 @@ def isitniceout():
     precipitation = ('rain' in response_dict or 'snow' in response_dict)
 
     nice_out_key = 'No'
-    if ((feels_like >= 40 and feels_like <= 82) and 
-        (wind_speed < 15) and
+    if ((feels_like >= 60 and feels_like <= 82) and
+        (wind_speed < 10) and
         (not precipitation)):
        nice_out_key = 'Yes'
 
